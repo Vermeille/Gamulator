e387d88e2bdd21af97c672191a76024c47f0dda9
Guillaume 'Vermeille' Sanchez <guillaume.v.sanchez@gmail.com>
2017-12-11 12:41:55 +0100

    add timer

---
+++ timer.h
@@ -0,0 +1,75 @@
+#pragma once
+
+#include "utils.h"
+
+class Timer {
+   public:
+    Timer()
+        : cnt_(uint8_t(0)),
+          cycle_(kCpuFreq / 16384),
+          div_cycle_cnt_(0),
+          tima_cycle_cnt_(0),
+          tima_(uint8_t(0)),
+          tma_(uint8_t(0)) {}
+
+    void Clock() {
+        int_ = false;
+        ++div_cycle_cnt_;
+        if (div_cycle_cnt_ == cycle_) {
+            ++cnt_.u;
+            div_cycle_cnt_ = 0;
+        }
+
+        if ((tac_.u & 0b100) == 0) {
+            return;
+        }
+
+        ++tima_cycle_cnt_;
+        if (tima_cycle_cnt_ >= kCpuFreq / TimerFreq()) {
+            tima_cycle_cnt_ = 0;
+            ++tima_.u;
+            if (tima_.u == 0) {
+                tima_.u = tma_.u;
+                int_ = true;
+            }
+        }
+    }
+
+    void Reset() { cnt_.u = 0; }
+    Data8 div() const { return cnt_; }
+
+    Data8 tima() const { return tima_; }
+    void set_tima(Data8 tima) { tima_ = tima; }
+
+    Data8 tma() const { return tma_; }
+    void set_tma(Data8 tma) { tma_ = tma; }
+
+    Data8 tac() const { return tac_; }
+    void set_tac(Data8 tac) { tac_ = tac; }
+
+    bool tima_int() const { return int_; }
+
+   private:
+    int TimerFreq() const {
+        switch (tac_.u & 0b11) {
+            case 0:
+                return 4096u;
+            case 1:
+                return 262144u;
+            case 2:
+                return 65536u;
+            case 3:
+                return 16384u;
+        }
+        assert(false);
+    }
+
+    Data8 cnt_;
+    const int cycle_;
+    int div_cycle_cnt_;
+    int tima_cycle_cnt_;
+    Data8 tima_;
+    Data8 tma_;
+    Data8 tac_;
+    bool int_;
+};

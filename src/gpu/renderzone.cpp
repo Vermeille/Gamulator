045d09532cbcf6c08b1b750a16609714ee348aca
Guillaume 'Vermeille' Sanchez <guillaume.v.sanchez@gmail.com>
2018-01-09 03:13:57 +0100

    Handle keys with SDL

--- src/gpu/renderzone.cpp
+++ src/gpu/renderzone.cpp
@@ -1,16 +1,6 @@
 #include "renderzone.h"
 
 void RenderZone::Render() {
-#if 0
-    sf::Event event;
-    while (_window.pollEvent(event)) {
-        // Request for closing the window
-        if (event.type == sf::Event::Closed) {
-            _window.close();
-            exit(0);
-        }
-    }
-#endif
     _tx.Update(&_pixels[0]);
     _win.Clear();
     _tx.Draw(_win);
--- src/keypad.h
+++ src/keypad.h
@@ -8,29 +8,54 @@ class Keypad {
    public:
     using byte = unsigned char;
 
+    Keypad() : _keys(0) {}
+
     void set_joyp(byte v) {
         _dir_keys = ((v >> 4) & 1) == 0;
         _buttons_keys = ((v >> 5) & 1) == 0;
     }
 
-    byte joyp() const {
+    byte joyp() {
+        RefreshKeys();
         if (_dir_keys) {
-            return ~(~0x1f |
-                     (sf::Keyboard::isKeyPressed(sf::Keyboard::Down) << 3) |
-                     (sf::Keyboard::isKeyPressed(sf::Keyboard::Up) << 2) |
-                     (sf::Keyboard::isKeyPressed(sf::Keyboard::Left) << 1) |
-                     (sf::Keyboard::isKeyPressed(sf::Keyboard::Right) << 0));
+            return ~(~0x1f | (_keys >> 4));
         } else {
-            return ~(
-                ~0x2f |
-                (sf::Keyboard::isKeyPressed(sf::Keyboard::Return) << 3) |
-                (sf::Keyboard::isKeyPressed(sf::Keyboard::BackSpace) << 2) |
-                (sf::Keyboard::isKeyPressed(sf::Keyboard::D) << 1) |
-                (sf::Keyboard::isKeyPressed(sf::Keyboard::S) << 0));
+            return ~(~0x2f | (_keys & 0xf));
         }
     }
 
+    bool poweroff() const { return _keys & 0x100; }
+
    private:
+    void RefreshKeys() {
+        SDL_Event event;
+        while (SDL_PollEvent(&event)) {
+            if (event.type == SDL_QUIT) {
+                exit(0);
+            }
+            if (event.type == SDL_KEYDOWN) {
+                _keys |= ((event.key.keysym.sym == SDLK_DOWN) << 7) |
+                         ((event.key.keysym.sym == SDLK_UP) << 6) |
+                         ((event.key.keysym.sym == SDLK_LEFT) << 5) |
+                         ((event.key.keysym.sym == SDLK_RIGHT) << 4) |
+                         ((event.key.keysym.sym == SDLK_RETURN) << 3) |
+                         ((event.key.keysym.sym == SDLK_BACKSPACE) << 2) |
+                         ((event.key.keysym.sym == SDLK_d) << 1) |
+                         ((event.key.keysym.sym == SDLK_s) << 0);
+            } else if (event.type == SDL_KEYUP) {
+                _keys &= ~(((event.key.keysym.sym == SDLK_DOWN) << 7) |
+                           ((event.key.keysym.sym == SDLK_UP) << 6) |
+                           ((event.key.keysym.sym == SDLK_LEFT) << 5) |
+                           ((event.key.keysym.sym == SDLK_RIGHT) << 4) |
+                           ((event.key.keysym.sym == SDLK_RETURN) << 3) |
+                           ((event.key.keysym.sym == SDLK_BACKSPACE) << 2) |
+                           ((event.key.keysym.sym == SDLK_d) << 1) |
+                           ((event.key.keysym.sym == SDLK_s) << 0));
+            }
+        }
+    }
+
     bool _dir_keys;
     bool _buttons_keys;
+    int _keys;
 };

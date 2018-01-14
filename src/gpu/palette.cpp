b5ab1c5449084b6898e523a7c21f08a4213607b7
Guillaume 'Vermeille' Sanchez <guillaume.v.sanchez@gmail.com>
2018-01-14 12:04:53 +0100

    slightly green palette to mimic GB display

--- src/gpu/palette.cpp
+++ src/gpu/palette.cpp
@@ -1,6 +1,6 @@
 #include "palette.h"
 
-const Color Palette::kColors[] = {Color(255, 255, 255),
+const Color Palette::kColors[] = {Color(225, 255, 225),
                                   Color(192, 192, 192),
-                                  Color(96, 96, 96),
-                                  Color(0, 0, 0)};
+                                  Color(96, 126, 96),
+                                  Color(0, 30, 0)};
